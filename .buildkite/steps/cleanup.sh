056a6ca94f9aafec2abafc7b5c4cc0e99a69fd52
Lachlan Donald <lachlan@ljd.cc>
2017-06-18 12:57:37 +1000

    Add a cleanup script to remove old stacks

--- .buildkite/steps/cleanup.sh
+++ .buildkite/steps/cleanup.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-set -eu
+# shellcheck disable=SC2016
+set -euxo pipefail
 
 stack_name=$(buildkite-agent meta-data get stack_name)
+cutoff_date=$(date --date='-2 days' +%Y-%m-%d)
 
 echo "--- Deleting stack $stack_name"
 aws cloudformation delete-stack --stack-name "$stack_name"
+
+echo "--- Deleting test managed secrets buckets created"
+aws s3api list-buckets \
+  --output text \
+  --query "$(printf '[?CreationDate<`%s`].Name' "$cutoff_date" )"
+
+echo "--- Deleting old cloudformation stacks"
+aws cloudformation describe-stacks \
+  --output text \
+  --query "$(printf 'Stacks[?LastUpdateTime<`%s`].StackName' "$cutoff_date" )"
